4f97bc66825df74f5ebedf954054a5f1de52e58d
MAFIACT <gregoire.pitrel@gmail.com>
2025-04-15 13:33:26 +0200

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -4,8 +4,16 @@ import { configureChains, createConfig } from 'wagmi'
 import { base } from 'wagmi/chains'
 import { publicProvider } from 'wagmi/providers/public'
 import { EthereumClient } from '@web3modal/ethereum'
+import { createWeb3Modal } from '@web3modal/wagmi'
 
 export const projectId = '465e1e1d0e40f88070f30ea5af2cd7fd'
+
+const metadata = {
+  name: 'Web3 Dashboard',
+  description: 'Connect Wallet & View PNL',
+  url: 'https://web3secondxork.vercel.app',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
 
 const { chains, publicClient } = configureChains([base], [publicProvider()])
 
@@ -15,3 +23,10 @@ export const wagmiConfig = createConfig({
 })
 
 export const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
+createWeb3Modal({
+  projectId,
+  chains,
+  wagmiConfig,
+  metadata
+})
